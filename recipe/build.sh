851b73a12b1aae77ae23bea4bb6ff98f79f17957
Julien Schueller <schueller@phimeca.com>
2026-04-28 18:12:10 +0200

    v1

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cmake ${CMAKE_ARGS} -DBUILD_STATIC_LIBS=OFF -DBUILD_TESTS=ON .
-make install -j${CPU_COUNT}
+cmake -G Ninja ${CMAKE_ARGS} -DBUILD_STATIC_LIBS=OFF -DBUILD_TESTS=ON -DWITH_MULTIARCH=OFF .
+cmake --build . --target install --parallel ${CPU_COUNT}
 if test "${host_alias}" == "${build_alias}"
 then
   ctest -j${CPU_COUNT} --output-on-failure
